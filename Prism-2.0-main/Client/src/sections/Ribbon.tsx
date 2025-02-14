319274567bf8684ef8a32c5ec0187e2c6161d458
ItsAyushRaman <164048863+ItsAyushRaman@users.noreply.github.com>
2025-02-14 14:56:31 +0530

    Logo Height Updated

--- Prism-2.0-main/Client/src/sections/Ribbon.tsx
+++ Prism-2.0-main/Client/src/sections/Ribbon.tsx
@@ -4,7 +4,7 @@ import LULogoNew from "../assets/LULogoNew.png";
 function Ribbon() {
   return (
     <div className=" bg-gradient-to-r from-[#200021] via-[#42006e] to-[#200021]  w-full h-[7vh]  flex  flex-row  justify-between items-center lg:px-10">
-      <img src={LULogoNew} alt="" className="h-[85%] md:h-[8.5vh] " />
+      <img src={LULogoNew} alt="" className="h-[70%] md:h-[7vh] " />
 
       <div className="flex flex-col items-center justify-center h-full text-white text-center">
       <p className="font-bold text-xl md:text-2xl xl:text-3xl text-center" style={{ fontFamily: 'Unbounded, sans-serif' }}>
@@ -16,7 +16,7 @@ function Ribbon() {
         </p>
       </div>
 
-      <img src={ribbonright} alt="" className="h-[97%] md:h-[8.5vh]" />
+      <img src={ribbonright} alt="" className="h-[80%] md:h-[7vh]" />
     </div>
   );
 }
