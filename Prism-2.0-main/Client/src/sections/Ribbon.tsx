132714503160fd2cde0fdf48baa7a52d5a9fde61
ItsAyushRaman <164048863+ItsAyushRaman@users.noreply.github.com>
2025-02-14 15:31:24 +0530

    new

--- Prism-2.0-main/Client/src/sections/Ribbon.tsx
+++ Prism-2.0-main/Client/src/sections/Ribbon.tsx
@@ -4,7 +4,7 @@ import LULogoNew from "../assets/LULogoNew.png";
 function Ribbon() {
   return (
     <div className=" bg-gradient-to-r from-[#200021] via-[#42006e] to-[#200021]  w-full h-[7vh]  flex  flex-row  justify-between items-center lg:px-10">
-      <img src={LULogoNew} alt="" className="h-[70%] md:h-[7vh] mr-4 md:mr-6 " />
+      <img src={LULogoNew} alt="" className="h-[70%] md:h-[7vh] mr-5 md:mr-7 " />
 
       <div className="flex flex-col items-center justify-center h-full text-white text-center">
       <p className="font-bold text-xl md:text-2xl xl:text-3xl text-center" style={{ fontFamily: 'Unbounded, sans-serif' }}>
